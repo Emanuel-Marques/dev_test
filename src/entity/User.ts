aecc8e67c23d62c60f186b38ffa9e19eb3c18895
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-17 23:33:40 +0100

    :sparkles: feat: add User entity with OneToMany relationship to Post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de User
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Post } from './Post';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  firstName?: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  lastName?: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email?: string;
+
+  @OneToMany(() => Post, post => post.user)
+  posts?: Post[];
+}
