f5713455cdee7bd4f2b886a2f204799f139e0943
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-17 23:34:18 +0100

    :sparkles: feat: add Post entity with relationship to User

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
 //TODO Crie a entidade de Post
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  title?: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  description?: string;
+
+  @Column()
+  userId?: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  user?: User;
+}
